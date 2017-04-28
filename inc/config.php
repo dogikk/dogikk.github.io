4074052b374289bdd9b74bcca665e4255d1e5f79
dogikk <dogik@protonmail.com>
2017-04-28 19:07:51 +0300

    Initial commit

---
+++ inc/config.php
@@ -0,0 +1,13 @@
+<?php
+$server = 'localhost';
+$user = 'botnet';
+$pass = 'sv4hsGHGHFGHF5656';
+$db = 'botnet';
+$correctuser = 'aGFja2VyNjY=';
+$correctpass = '5bceaea8a87ef084ae4006bfdd12d5d8';
+$usa = "E93A62B02899AE8F8D37372BF57A294B";
+$RC4 = "ghrujhdgjfhkdghfsjhdghjgfdhgjh";
+$folder = "009c434cab";
+mysql_connect($server, $user, $pass);
+mysql_select_db($db);
+?>
